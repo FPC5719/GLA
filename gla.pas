86d35b287e282032a06ed0405752db3ec2d9729a
NingBoChenChen <chenchen20050719@126.com>
2019-03-11 14:49:44 +0800

    Something

---
+++ gla.pas
@@ -0,0 +1,42 @@
+{
+GL Addons
+对GL、GLU中的东西进行了一定的整合
+（或许）可以简化使用
+FPC5719
+2019.2
+}
+{$MODE OBJFPC}
+unit gla;
+
+interface
+
+uses
+	gl,glu,glext,windows,
+	gMap,gUtil,
+	fpImage,fpReadBMP,fpReadJPEG,fpReadPNG,fpReadGIF;
+
+{$IFNDEF GLA_INTERFACE}
+{$DEFINE GLA_INTERFACE}
+{$ENDIF}
+{$IFDEF GLA_IMPLEMENTATION}
+{$UNDEF GLA_IMPLEMENTATION}
+{$ENDIF}
+
+{$INCLUDE gla_base.inc}
+{$INCLUDE gla_camera.inc}
+{$INCLUDE gla_texture.inc}
+
+implementation
+
+{$IFNDEF GLA_IMPLEMENTATION}
+{$DEFINE GLA_IMPLEMENTATION}
+{$ENDIF}
+{$IFDEF GLA_INTERFACE}
+{$UNDEF GLA_INTERFACE}
+{$ENDIF}
+
+{$INCLUDE gla_base.inc}
+{$INCLUDE gla_camera.inc}
+{$INCLUDE gla_texture.inc}
+
+end.
